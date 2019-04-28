8a814fef85d87216960fb0aaaf6a42490031252a
Mohit Malhotra <mmalhotra2000@gmail.com>
2019-04-28 17:12:18 +0530

    Zuul added

---
+++ zuul-server-mohit/src/main/java/com/gs/ilp/filter/zuulservermohit/MyZuulFilter.java
@@ -0,0 +1,48 @@
+package com.gs.ilp.filter.zuulservermohit;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class MyZuulFilter extends ZuulFilter{
+	
+	Logger logger = LoggerFactory.getLogger(MyZuulFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("Request details " + request + " request info " + request.getRequestURI());
+
+		
+		
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return FilterConstants.PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+}
