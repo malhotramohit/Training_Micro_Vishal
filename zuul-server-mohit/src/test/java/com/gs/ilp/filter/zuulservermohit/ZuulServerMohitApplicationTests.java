b7147940f54df0265e5b4fc540b0556a815781ea
Mohit Malhotra <mmalhotra2000@gmail.com>
2019-04-28 13:28:18 +0530

    Adding Zuul Server

---
+++ zuul-server-mohit/src/test/java/com/gs/ilp/filter/zuulservermohit/ZuulServerMohitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gs.ilp.filter.zuulservermohit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZuulServerMohitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
